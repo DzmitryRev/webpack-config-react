78c679f282a480810d96259d8ce4c7f2bc12bc07
unknown <revchenkodzmitry@gmail.com>
2021-12-29 18:09:57 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App  = () => {
+    let a: string = "Привет"
+    console.log(a)
+  return (
+    <>
+      <div>asasdasdasd</div>
+      <h1>asdasdasd</h1>
+    </>
+  );
+};
+
+export default App;
